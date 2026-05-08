d568b84841482fc6f6aabfb535bd0b8a3126ba07
vrmanideep <vrmanideepphaniharam@gmail.com>
2026-05-08 22:42:37 +0530

    chore: bumped version from 1.1.2 to 1.1.3

--- app/src/main/java/com/vtop/utils/AuthHelper.kt
+++ app/src/main/java/com/vtop/utils/AuthHelper.kt
@@ -8,10 +8,14 @@ import com.google.android.gms.common.api.Scope
 
 object AuthHelper {
     fun getGoogleSignInClient(context: Context, webClientId: String): GoogleSignInClient {
+
+        // PASTE YOUR EXACT CLIENT ID HERE FOR NOW
+        val hardcodedClientId = "1006105677727-utmqc9263kj1cfmupvpk2tv6vi8nus10.apps.googleusercontent.com"
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestScopes(Scope("https://www.googleapis.com/auth/gmail.readonly"))
-            .requestIdToken(webClientId)
+            .requestIdToken(hardcodedClientId)
             .build()
 
         return GoogleSignIn.getClient(context, gso)
