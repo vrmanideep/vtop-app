c72467b795b46c6c3e5a389e581438e6a324fcee
vrmanideep <vrmanideepphaniharam@gmail.com>
2026-05-08 22:42:24 +0530

    chore: bumped version from 1.1.2 to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.1.2"
+        versionName = "1.1.3"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
